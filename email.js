b9fe803995e9b0a860c9915044b3b20cb196e48c
Roo Williams <roo@roowilliams.com>
2020-10-12 21:33:48 -0700

    sending emails

--- email.js
+++ email.js
@@ -5,8 +5,11 @@ require('dotenv').config()
 const fs = require('fs')
 const chalk = require('chalk')
 const parse = require('csv-parse')
+const mailgun = require('mailgun-js')({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+})
 import config from './config.js'
-// const api = new EasyPost(process.env.EASYPOST_API_KEY)
 
 const Emailer = () => {
   var rows = []
@@ -27,6 +30,23 @@ const Emailer = () => {
     })
   }
 
+  function sendMail(order) {
+    var data = {
+      from: 'EAT BITTER <hello@eatbitter.co>',
+      to: order.email,
+      subject: 'Shipping Update',
+      text: 'Testing some Mailgun awesomness!',
+      template: 'eb_shipping_update', //Instead of 'html'
+      'v:trackingCode': order.trackingCode,
+      'v:trackingUrl': order.trackingUrl
+    }
+    return new Promise((resolve, reject) =>
+      mailgun.messages().send(data, function (error, body) {
+        if (error) reject(error)
+        setTimeout(() => resolve(body), 3000)
+      })
+    )
+  }
   function onComplete() {
     console.log('Process complete.')
   }
@@ -38,18 +58,17 @@ const Emailer = () => {
     if (currentRow === rows.length - 1) return onComplete()
     return updateRow()
   }
-  async function processRow(index) {
-    return new Promise((resolve, reject) => {
-      const row = rows[index]
-      // do stuff
-      let orderNumber = row['ORDER NUMBER']
-      let quantity = row['QUANTITY']
-      let email = row['CUSTOMER EMAIL']
-      let trackingCode = row['TRACKING CODE']
-      let trackingUrl = row['URL']
 
-      setTimeout(() => resolve(console.log(row)), 2000)
-    })
+  async function processRow(index) {
+    const row = rows[index]
+    // do stuff
+    let orderNumber = row['ORDER NUMBER']
+    let quantity = row['QUANTITY']
+    let email = row['CUSTOMER EMAIL']
+    let trackingCode = row['TRACKING CODE']
+    let trackingUrl = row['URL']
+    const order = { orderNumber, quantity, email, trackingCode, trackingUrl }
+    return sendMail(order).then(console.log)
   }
 
   async function beginProcessing() {
